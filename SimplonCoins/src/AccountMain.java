22f3eb7ae75b0aac874a458d7d903ba6a2e33d25
ATournel <a.tournel@hotmail.fr>
2018-07-31 16:34:48 +0200

    Adding a little bit more ux

--- SimplonCoins/src/AccountMain.java
+++ SimplonCoins/src/AccountMain.java
@@ -9,22 +9,33 @@ public class AccountMain extends Account {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Indiquez combien vous souhaitez virer à "+compteCourantBeneficiaire.proprietaire+":");
 		montant=scan.nextDouble();
-		solde = solde-montant;
-		compteCourantBeneficiaire.setSolde(compteCourantBeneficiaire.solde+montant);
-		System.out.println("Merci Aurelia, votre nouveau solde est de: "+solde+"€.");
+		if(montant>solde) {
+			System.out.println("Le montant choisi est superieur à votre solde.");
+		}
+		else {
+			solde = solde-montant;
+			compteCourantBeneficiaire.setSolde(compteCourantBeneficiaire.solde+montant);
+			System.out.println("Merci Aurelia, votre nouveau solde est de: "+solde+"€.");
+		}
 		
-		
+			
 		
 	}
 
 	public void virementPerso(double montant, AccountPEL compteCourantBeneficiaire) {
 		
 		Scanner scan = new Scanner(System.in);
-		
+		System.out.println("Indiquez combien vous souhaitez virer :");
 		montant=scan.nextDouble();
-		solde = solde-montant;
-		compteCourantBeneficiaire.setSolde(compteCourantBeneficiaire.solde+montant);
-		System.out.println("Merci Aurelia, votre nouveau solde courant est de: "+solde+"€.\nLe solde de votre plan epargne est de: "+solde+".€");
+		if(montant>solde) {
+			System.out.println("Le montant choisi est superieur à votre solde.");
+		}
+		else {
+			solde = solde-montant;
+			compteCourantBeneficiaire.setSolde(compteCourantBeneficiaire.solde+montant);
+			System.out.println("Merci Aurelia, votre nouveau solde est de: "+solde+"€.\nLe solde de votre plan epargne est de: "+compteCourantBeneficiaire.solde+".€");
+			
+		}
 		
 		
 		
--- SimplonCoins/src/Application.java
+++ SimplonCoins/src/Application.java
@@ -4,9 +4,9 @@ public class Application {
 
 	private static double montant;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner scan2 = new Scanner(System.in);
-		
+		int choix=0;
 		
 		AccountMain compteCourantP = new AccountMain();
 		AccountMain compteCourantA = new AccountMain();
@@ -21,18 +21,49 @@ public class Application {
 		planEpargneA.setSolde(1000.00);
 		planEpargneA.setTauxInterets(4.5);
 		
-		System.out.println("Bonjour Aurelia, votre solde actuel est de: "+compteCourantA.solde+"€.");
-		System.out.println("\n\nA qui souhaitez-vous faire un virement?");
-		System.out.println("\nTapez 1 pour votre plan epargne,\nTapez 2 pour votre beneficiaire favori.");
-		int choix = scan2.nextInt();
-		
-		if(choix==2) {
-		
-		compteCourantA.virement(montant, compteCourantP);
-		}
-		else {
-		compteCourantA.virementPerso(montant, planEpargneA);
-		}
+		System.out.println("Bonjour Aurelia,quelle operation souhaitez-vous faire?\n");
+		do {
+			System.out.println("Consulter mes comptes - 1\nEffectuer un virement - 2\nContacter mon agence  - 3\nDeconnexion           - 4");
+			choix = scan2.nextInt();
+			
+			if(choix == 1) {
+				System.out.println("vos soldes actuels sont de:\nCompte courant: "+compteCourantA.solde+"€.\nPlan epargne: "+planEpargneA.solde+"€.");
+				System.out.println("Retour au menu.\n....\n\n\n");
+				Thread.sleep(2000);
+			}
+			else if(choix==2) {
+				System.out.println("Vers quel compte souhaitez-vous faire un virement?\nPlan epargne - 1\nBeneficiaire favori - 2");
+				int choix2 = scan2.nextInt();
+				
+				if(choix2==2) {				
+				compteCourantA.virement(montant, compteCourantP);
+				System.out.println("Retour au menu.\n....\n\n\n");
+				Thread.sleep(2000);
+				}
+				else if(choix2==1){
+				compteCourantA.virementPerso(montant, planEpargneA);
+				System.out.println("Retour au menu.\n....\n\n\n");
+				Thread.sleep(2000);
+				}
+				else {
+					System.out.println("Commande inconnue.\nRetour au menu.\n....\n\n\n");
+					Thread.sleep(2000);
+				}
+			}
+			else if(choix==3) {
+				System.out.println("Votre agence est fermée, contact possible dès Lundi 8h30.");
+				System.out.println("Retour au menu.\n....\n\n\n");
+				Thread.sleep(2000);
+			}
+			else if(choix==4) {
+				System.out.println("Deconnexion...");
+			}
+			else {
+				System.out.println("Commande inconnue.\\nRetour au menu.\n....\n\n\n");
+				Thread.sleep(2000);
+			}
+		}while(choix!=4);
+			
 		
 		
 	}
